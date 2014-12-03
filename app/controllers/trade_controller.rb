acd17c2903f912582995137732eeb6db9c3f41e3
Steven DiFrancesco <algostavo@gmail.com>
2014-12-03 14:59:24 -0500

    updated trade controller

--- app/controllers/trade_controller.rb
+++ app/controllers/trade_controller.rb
@@ -14,6 +14,89 @@ require 'rufus-scheduler'
 
 	def execute
 
+		domain = 'https://api-fxpractice.oanda.com'
+		accountId = '900859'
+		instrument = 'EUR_USD'    
+		access_token = '92eb2d8911829720802dda141031945b-942982d88fae70ac1316cc77d1014b4e'
+
+
+		priceTodayH  = @rateTodayH
+		priceUpToday = @rateTodayC if @rateTodayC > @rateTodayO
+		priceDnToday = @rateTodayC if @rateTodayC < @rateTodayO
+		periodHigh   = @rateHigh
+		periodLow    = @rateLow
+		range   = periodHigh - periodLow
+		leveL1  = 0.10.to_f * range + periodLow
+		leveL3  = 0.33.to_f * range + periodLow
+		leveL5  = 0.50.to_f * range + periodLow
+		leveL6  = 0.66.to_f * range + periodLow
+		leveL8  = 0.80.to_f * range + periodLow
+		leveL88 = 0.88.to_f * range + periodLow
+
+
+
+		if priceUpToday and priceTodayH.between?(leveL3, leveL5)
+			paid   = leveL1
+		   stop   = paid - (paid * 0.005.to_f) 	# adjust later based on current day volitility?
+		   profit = paid + (paid * 0.015.to_f)  	# actively adjust based on real-time orderbook flow? 
+		   expiry = 5.days.from_now.strftime('%Y-%m-%dT%H:%M:%S.%fZ')
+		   uri    = URI.parse(URI.encode(domain + '/v1/accounts/' + accountId + '/orders' ))
+		   params = URI.escape('instrument=' + instrument + '&units=1' + '&side=buy' + '&type=limit' + '&expiry=' + expiry + '&price=' + paid + '&stopLoss=' + stop + '&takeProfit=' + profit )
+		   Net::HTTP.start(uri.host, uri.port, :use_ssl => uri.scheme == 'https') do |http|
+		   	request = Net::HTTP::Post.new(uri)
+		   	request.body = params             
+		   	request['Authorization'] = 'Bearer ' + access_token                
+		   	response = http.request(request)
+		   	puts response.body 
+		   	sleep(1.days)
+		   elsif priceTodayH > leveL6 and priceUpToday.between?(leveL5, leveL8)
+		   	paid   = leveL5
+		   	stop   = paid - (paid * 0.005.to_f) 	
+		   	profit = paid + (paid * 0.015.to_f)  	
+		   	expiry = 5.days.from_now.strftime('%Y-%m-%dT%H:%M:%S.%fZ')
+		   	uri    = URI.parse(URI.encode(domain + '/v1/accounts/' + accountId + '/orders' ))
+		   	params = URI.escape('instrument=' + instrument + '&units=1' + '&side=buy' + '&type=limit' + '&expiry=' + expiry + '&price=' + paid + '&stopLoss=' + stop + '&takeProfit=' + profit )
+		   	Net::HTTP.start(uri.host, uri.port, :use_ssl => uri.scheme == 'https') do |http|
+		   		request = Net::HTTP::Post.new(uri)
+		   		request.body = params             
+		   		request['Authorization'] = 'Bearer ' + access_token                
+		   		response = http.request(request)
+		   		puts response.body 
+		   		sleep(1.days)
+		   	elsif priceUpToday and priceTodayH > leveL8
+		   		paid   = leveL6
+		   		stop   = paid - (paid * 0.005.to_f) 	
+		   		profit = paid + (paid * 0.015.to_f)  	
+		   		expiry = 5.days.from_now.strftime('%Y-%m-%dT%H:%M:%S.%fZ')
+		   		uri    = URI.parse(URI.encode(domain + '/v1/accounts/' + accountId + '/orders' ))
+		   		params = URI.escape('instrument=' + instrument + '&units=1' + '&side=buy' + '&type=limit' + '&expiry=' + expiry + '&price=' + paid + '&stopLoss=' + stop + '&takeProfit=' + profit )
+		   		Net::HTTP.start(uri.host, uri.port, :use_ssl => uri.scheme == 'https') do |http|
+		   			request = Net::HTTP::Post.new(uri)
+		   			request.body = params             
+		   			request['Authorization'] = 'Bearer ' + access_token                
+		   			response = http.request(request)
+		   			puts response.body 
+		   			sleep(1.days)			
+		   		elsif priceDnToday.between?(leveL3, leveL5) and priceTodayH < leveL8
+		   			paid   = leveL88
+		   			stop   = paid + (paid * 0.005.to_f) 	
+		   			profit = paid - (paid * 0.015.to_f)  	
+		   			expiry = 5.days.from_now.strftime('%Y-%m-%dT%H:%M:%S.%fZ')
+		   			uri    = URI.parse(URI.encode(domain + '/v1/accounts/' + accountId + '/orders' ))
+		   			params = URI.escape('instrument=' + instrument + '&units=1' + '&side=sell' + '&type=limit' + '&expiry=' + expiry + '&price=' + paid + '&stopLoss=' + stop + '&takeProfit=' + profit )
+		   			Net::HTTP.start(uri.host, uri.port, :use_ssl => uri.scheme == 'https') do |http|
+		   				request = Net::HTTP::Post.new(uri)
+		   				request.body = params             
+		   				request['Authorization'] = 'Bearer ' + access_token                
+		   				response = http.request(request)
+		   				puts response.body 
+		   				sleep(1.days)		      
+		   		  end
+		   	   	end
+		   	  end
+		   end
+	     end 
+	   end											
 	end
 
 
@@ -44,7 +127,6 @@ require 'rufus-scheduler'
 	          	tradeGroup = JSON.parse(result)['trades']
 
 	          		@tradeData = tradeGroup.flat_map(&:to_a).select{ |k,v| ["time", "instrument", "side", "price", "takeProfit", "stopLoss"].include?(k)}
-	          			#render "view", :tradeData => @tradeData
     		end
  		end
 	end
@@ -79,7 +161,6 @@ require 'rufus-scheduler'
 	          	tradeGroupHistory = JSON.parse(result)['transactions']
 
 	          		@tradeDataHistory = tradeGroupHistory.flat_map(&:to_a).select{ |k,v| ["id", "time", "type" "instrument", "side", "price", "takeProfitPrice", "stopLossPrice", "pl", "interest"].include?(k)}
-	          			#render "view", :tradeDataHistory => @tradeDataHistory
 	    	end
 	 	end
 	end		
